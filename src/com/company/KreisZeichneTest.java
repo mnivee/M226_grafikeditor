37daee21aa7a4a0c036080264b769f689273d161
Niveditha <niveditha_1999@hotmail.com>
2020-01-30 10:26:54 +0100

    done with this modul

--- src/com/company/KreisZeichneTest.java
+++ src/com/company/KreisZeichneTest.java
@@ -8,9 +8,11 @@ import static junit.framework.TestCase.assertEquals;
 public class KreisZeichneTest {
     private GraphicStub g2 = new GraphicStub();
     private Kreis kreis;
+    private Rechteck r;
 
     @Before
     public void setup() {
+        r = new Rechteck("Rechteck", 10, 10, 30, 70);
         kreis = new Kreis("Kreis", 50, 80, 90);
     }
 
@@ -22,5 +24,9 @@ public class KreisZeichneTest {
         assertEquals(80, g2.y);
         assertEquals(90, g2.width);
         assertEquals(90, g2.height);
+        r.zeichne(g2);
+        assertEquals(10, g2.);
+        assertEquals(1, g2.numOfDrawOvalCalls);
+
     }
 }
